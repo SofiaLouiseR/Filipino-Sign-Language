17fee9c7879582dd3d832b3b01683b6e9f021c71
SofiaLouiseR <57057789+SofiaLouiseR@users.noreply.github.com>
2022-05-25 17:08:08 +0800

    Deleted test files

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, Response
-from camera import Video
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,17 +13,6 @@ def learn():
 @app.route('/practice')
 def practice():
     return render_template("practice.html")
-
-def gen(camera):
-    while True:
-        frame=camera.get_frame()
-        yield(b'--frame\r\n'
-       b'Content-Type:  image/jpeg\r\n\r\n' + frame +
-         b'\r\n\r\n')
-
-def video():
-    return Response(gen(Video()),
-    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/about")
 def about():
